24d20a6c241d83ea0c0627bdf51e876020cdaaf5
internetcharles <charlesodellsmith@gmail.com>
2020-05-18 12:43:05 -0700

    added scriptable objects and state

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "You walk into a bank, a man in a stained brown duster asks you if you'd like into invest in the \"future of beans\"" +
+            "Do you:" +
+            "1. Give him $500" +
+            "2. Tell him to fuck off.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
